8257cdd26f0c4f842defc2893961ade7bd49498e
saswat-sahay <saswatsahay@gmail.com>
2025-11-18 12:30:56 +0530

    learned exception handling

--- 8_exceptionHandling.py
+++ 8_exceptionHandling.py
@@ -1,5 +1,12 @@
 # syntax error, indentation error are not handled by exception handling
 
+# try       Wrap the block of code that might cause an exception.
+# except    Handle the exception if it occurs
+# else      Run code only if no exception occurs
+# finally   Run code no matter what, whether there's an exception or not
+# raise     Manually throw an exception
+
+
 # zero division error
 
 a = int(input("enter number: "))
@@ -14,4 +21,31 @@ except ZeroDivisionError:
 
 print("division completed")
 
-# now since we 
+# now since we dont have only zero division error
+# lets go for a general method for handling exceptions
+
+a = int(input("enter number: "))
+
+try:
+    print(10/a)
+except Exception as err:
+    print(f"sorry this is an {err}")
+
+# else - runs if except didnot run
+else:
+    print("good no exception")
+
+finally:
+    print("it will run always")
+
+# raise
+
+age = int(input("enter age"))
+try:
+    if age<18:
+        raise ValueError("your age must be greater than 18")
+    else:
+        print("welocme to the voterlist")
+except Exception as err:
+    print(f"the error is {err}")
+print("the voting will start soon")
